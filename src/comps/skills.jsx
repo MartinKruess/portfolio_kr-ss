488db21241528d01f26fd4b701b01237414ea126
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2022-11-07 15:35:59 +0100

    content and translation 01

--- src/comps/skills.jsx
+++ src/comps/skills.jsx
@@ -64,7 +64,7 @@ export const Skills = () => {
                     <br /> 
                     {language ? ("The values ​​given here show how confident I feel in my previous tasks with the respective techniques.") : ("Die hier angegebenen Werte zeigen, wie sicher ich mich bei meinen bisherigen Aufgaben mit den jeweiligen Techniken fühle.")}
                     <br />
-                    fine > 90% - good > 80% - okay > 60% - basics > 20%"
+                    fine > 90% - good > 80% - okay > 60% - basics > 20%
                 </p>
                 { skills.map((skill, i) => (
                 <div className="card" key={i}>
